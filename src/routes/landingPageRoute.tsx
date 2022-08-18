e51345cdf3e4e2e0a5991405f17e6f3f41983a32
Vincent Selhorst-Jones <vselhorst-jones@chanzuckerberg.com>
2022-08-18 16:08:36 -0700

    try try again, change that route

--- src/routes/landingPageRoute.tsx
+++ src/routes/landingPageRoute.tsx
@@ -2,6 +2,11 @@ import Header from "../components/header";
 import LandingPage from "../components/landingPage";
 
 export const LandingPageRoute = () => {
+  console.log("Hello from the landing page"); // REMOVE
+  console.log("impor meta env MODE", import.meta.env.MODE); // REMOVE
+  console.log("impor meta env BASE_URL", import.meta.env.BASE_URL); // REMOVE
+  console.log("impor meta env PROD", import.meta.env.PROD); // REMOVE
+  console.log("impor meta env SSR", import.meta.env.SSR); // REMOVE
   return (
     <div>
       <Header />

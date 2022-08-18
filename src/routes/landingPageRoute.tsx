86a6ecbed068ee9cc88d039dedc50b2259964476
Vincent Selhorst-Jones <vselhorst-jones@chanzuckerberg.com>
2022-08-18 16:29:38 -0700

    Tweak the ending slash

--- src/routes/landingPageRoute.tsx
+++ src/routes/landingPageRoute.tsx
@@ -2,7 +2,7 @@ import Header from "../components/header";
 import LandingPage from "../components/landingPage";
 
 export const LandingPageRoute = () => {
-  console.log("hey again, this time put in path forward slashes"); // REMOVE
+  console.log("heyx3, dropped base path final slash"); // REMOVE
   console.log("impor meta env MODE", import.meta.env.MODE); // REMOVE
   console.log("impor meta env BASE_URL", import.meta.env.BASE_URL); // REMOVE
   console.log("impor meta env PROD", import.meta.env.PROD); // REMOVE

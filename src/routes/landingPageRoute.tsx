30b3278163a32696bdc84d019486d24fb3d59b31
Vincent Selhorst-Jones <vselhorst-jones@chanzuckerberg.com>
2022-08-19 16:54:21 -0700

    Think we got that secret locked up folks

--- src/routes/landingPageRoute.tsx
+++ src/routes/landingPageRoute.tsx
@@ -2,6 +2,8 @@ import Header from "../components/header";
 import LandingPage from "../components/landingPage";
 
 export const LandingPageRoute = () => {
+  console.log("This version should have built with flag?"); // REMOVE
+  console.log("importXmetaXenvXMODE", import.meta.env.MODE); // REMOVE
   return (
     <div>
       <Header />

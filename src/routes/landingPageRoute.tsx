5f2340f031a412c8578ae4b0b63b6ff8be54defe
Vincent Selhorst-Jones <vselhorst-jones@chanzuckerberg.com>
2022-08-19 17:15:17 -0700

    Okay, one more fiddle, for the road

--- src/routes/landingPageRoute.tsx
+++ src/routes/landingPageRoute.tsx
@@ -2,7 +2,7 @@ import Header from "../components/header";
 import LandingPage from "../components/landingPage";
 
 export const LandingPageRoute = () => {
-  console.log("This version should have built with flag?"); // REMOVE
+  console.log("Let us scope out that multi line secret, friend!"); // REMOVE
   console.log("importXmetaXenvXMODE", import.meta.env.MODE); // REMOVE
   return (
     <div>

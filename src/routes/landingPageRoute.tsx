a2421af8d375ca9f325b305c7aa078f58cc41c71
Vincent Selhorst-Jones <vselhorst-jones@chanzuckerberg.com>
2022-08-19 17:41:09 -0700

    Wrap up, only build/deploy on change to main

--- src/routes/landingPageRoute.tsx
+++ src/routes/landingPageRoute.tsx
@@ -2,8 +2,6 @@ import Header from "../components/header";
 import LandingPage from "../components/landingPage";
 
 export const LandingPageRoute = () => {
-  console.log("Let us scope out that multi line secret, friend!"); // REMOVE
-  console.log("importXmetaXenvXMODE", import.meta.env.MODE); // REMOVE
   return (
     <div>
       <Header />

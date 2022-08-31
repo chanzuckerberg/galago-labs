43a6b598be3f6175e4874249a5c7dd6f917eae69
Sidney Bell <sidneymbell@gmail.com>
2022-08-30 21:56:20 -0700

    Split selectors into separate subcomponents. Start styling work (WIP)

--- src/components/cladeFilterDrawer/caseDefinitionConstructor/index.tsx
+++ src/components/cladeFilterDrawer/caseDefinitionConstructor/index.tsx
@@ -3,13 +3,16 @@ import { useState } from "react";
 import Selectors from "./selectors";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
-import { Button } from "@mui/material";
+import { Button, FormLabel } from "@mui/material";
+import Theme from "../../../theme";
+import ContinuousDataSlider from "./continuousDataSlider";
+import CategoricalDataSelector from "./categoricalDataSelector";
 
 export const CaseDefinitionConstructor = () => {
   // @ts-ignore -- TODO: figure out how to add types to state
   const state = useSelector((state) => state.global);
   const dispatch = useDispatch();
-  const [selectedFields, setSelectedFields] = useState<Set<string>>(new Set());
+  const [selectedFields, setSelectedFields] = useState<string[]>([]);
 
   const fieldIsValid = (fieldSummary: any) => {
     if (fieldSummary["dataType"] == "categorical") {
@@ -33,55 +36,106 @@ export const CaseDefinitionConstructor = () => {
   });
 
   return (
-    <div style={{ marginTop: 30, marginBottom: 50 }}>
-      <h3>Case definition</h3>
-      <p style={{ fontStyle: "italic", fontSize: 14 }}>
-        Optionally, add samples of interest based on a case definition.
-      </p>
-      <Autocomplete
-        multiple
-        id="tags-outlined"
-        style={{ paddingTop: 15, width: 250 }}
-        options={validFields}
-        getOptionLabel={(option: any) => option.label}
-        limitTags={5}
-        disableCloseOnSelect
-        size="small"
-        // BUG: set is a temp workaround for the fact that filterSelectedOptions doesn't work such that the same variable can be selected more than once
-        filterSelectedOptions
-        onChange={(event: any, newValue: any) => {
-          let newFields = new Set();
-          newValue.forEach((nv: { label: string; id: number }) =>
-            newFields.add(nv["label"])
-          );
-          //@ts-ignore - all the labels are strings
-          setSelectedFields(newFields); //newValue?.map((nv: any) => nv["label"]));
+    <div // TODO: this a column flex div with the two buttons together in a div/row at the bottom
+      style={{
+        //@ts-ignore
+        backgroundColor: Theme.palette.secondary.lighter,
+        padding: 10,
+        borderRadius: 5,
+      }}
+    >
+      <h4 style={{ margin: 0 }}>Add samples by case definition:</h4>
+      <FormLabel>
+        Samples matching selected metadata will be added to the list of Sample
+        Names.
+      </FormLabel>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
         }}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Select metadata fields"
-            // placeholder="Favorites"
-          />
-        )}
-      />
-      {selectedFields && <Selectors selectedFields={selectedFields} />}
-      <Button
-        disableElevation
-        disableRipple
-        style={{ marginTop: 15 }}
-        variant="contained"
-        name="submitCaseDef"
-        onClick={(e) => {
-          dispatch({
-            type: "case definition submitted",
-          });
-        }}
-        size="small"
-        disabled={Object.keys(state.caseDefFilters).length === 0}
       >
-        Add matches to Samples of Interest
-      </Button>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <Autocomplete
+            multiple
+            id="tags-outlined"
+            style={{ paddingTop: 15, width: 250 }}
+            options={validFields}
+            getOptionLabel={(option: any) => option.label}
+            limitTags={5}
+            disableCloseOnSelect
+            size="small"
+            filterSelectedOptions
+            onChange={(event: any, newValue: any) => {
+              setSelectedFields(newValue.map((nv: any) => nv.label));
+            }}
+            isOptionEqualToValue={(opt, value) => {
+              return opt.label === value.label;
+            }}
+            renderInput={(params) => (
+              <TextField {...params} label="Select metadata" />
+            )}
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-evenly",
+          }}
+        >
+          {selectedFields.map((field: string) => {
+            return state.metadataCensus[field]["dataType"] === "continuous" ? (
+              <ContinuousDataSlider field={field} />
+            ) : (
+              <CategoricalDataSelector field={field} />
+            );
+          })}
+        </div>
+      </div>
+      <div
+        style={
+          {
+            // display: "flex",
+            // flexDirection: "row",
+            // justifyContent: "space-around",
+          }
+        }
+      >
+        <Button
+          disableElevation
+          disableRipple
+          style={{ marginTop: 30 }}
+          variant="contained"
+          name="submitCaseDef"
+          onClick={(e) => {
+            dispatch({
+              type: "case definition submitted",
+            });
+          }}
+          size="small"
+          disabled={Object.keys(state.caseDefFilters).length === 0}
+        >
+          Add matches to Samples of Interest
+        </Button>
+        <Button
+          variant="text"
+          style={{ marginTop: 30 }}
+          onClick={() => {
+            setSelectedFields([]);
+            dispatch({ type: "case definition filters cleared" });
+          }}
+        >
+          CANCEL
+        </Button>
+      </div>
     </div>
   );
 };

82011f2e9f86686d03cdd6fa190828fff2495b4d
Vincent Selhorst-Jones <vselhorst-jones@chanzuckerberg.com>
2022-09-23 09:23:50 -0700

    WIP: Search param handling, parsing

--- src/routes/fetchTree.tsx
+++ src/routes/fetchTree.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import axios from "axios";
-import { getUrlToFetch } from "../utils/fetchData";
+import { getTargetUrlAndParams } from "../utils/fetchData";
 import LandingPageRoute from "./landingPageRoute";
 import { ACTION_TYPES } from "../reducers/actionTypes";
 import {
--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -4,6 +4,59 @@
 import { ROUTES } from "../routes";
 
 /**
+ * VOODOO_TODO__DOC_ME
+ */
+const SEARCH_PARAM_PREFIX = "galago";
+const ALL_GALAGO_PARAMS = [
+  'pathogen', // corresponding URL search param: `galagoPathogen`
+  'mrca', // corresponding URL search param: `galagoMrca`
+] as const;
+type AllGalagoParams = typeof ALL_GALAGO_PARAMS[number];
+// For actual usage downstream in app, we provide an object with the params as
+// keys, with the string value it had or `undefined` if not represented in URL.
+type GalagoParams = Partial<Record<AllGalagoParams, string>>
+// Helper to do internal name to URL search param: `fooBar` => `galagoFooBar`
+const internalToSearchParam = (param: string): string => {
+  return SEARCH_PARAM_PREFIX + param.charAt(0).toUpperCase() + param.slice(1);
+};
+
+interface ExtractedSearchParams {
+  galagoParams: GalagoParams;
+  remainingSearchString: string;
+}
+
+/**
+ * VOODOO_TODO__DOC_ME
+ */
+function extractSearchParams(searchString: string | undefined): ExtractedSearchParams {
+  const galagoParams: ExtractedSearchParams["galagoParams"] = {};
+  if (!searchString) { // There was no search portion. Just return defaults.
+    return {
+      galagoParams,
+      remainingSearchString: "",
+    };
+  }
+  // Fetch did have search params, handle them now.
+  const searchParams = new URLSearchParams(searchString);
+  ALL_GALAGO_PARAMS.forEach((internalParam) => {
+    const searchParamName = internalToSearchParam(internalParam);
+    const paramValue = searchParams.get(searchParamName);
+    if (paramValue !== null) {
+      galagoParams[internalParam] = paramValue;
+      searchParams.delete(searchParamName);
+    }
+  })
+  // At this point, whatever remains in `searchParams` was not for Galago's use
+  // If we used all of the search params, this will be an empty string.
+  const remainingSearchString = searchParams.toString();
+  return {
+    galagoParams,
+    remainingSearchString,
+  };
+}
+
+
+/**
  * If passed string does not have an http(s) schema already, prefix with https.
  *
  * We allow user to specify the URL they want a tree JSON fetched from, but we
@@ -23,6 +76,11 @@ function schemifyUrl(rawUrl: string): string {
   return result;
 }
 
+
+interface TargetUrlAndParams {
+  targetUrl: string; // Full URL for where we should go get JSON data
+  galagoParams: GalagoParams; // Set of (optional) query params about tree
+}
 /**
  * Gets the URL for external JSON tree from browser's current location.
  *
@@ -56,20 +114,40 @@ function schemifyUrl(rawUrl: string): string {
  * test where this would get called as part of it, look into "mocking window"
  * for javascript testing.
  */
-export function getUrlToFetch(): string {
+export function getTargetUrlAndParams(): TargetUrlAndParams {
+  // Return value for this func when fetch path was accessed incorrectly.
+  const MALFORMED_FETCH_RETURN_VAL: TargetUrlAndParams = {
+    targetUrl: "",
+    galagoParams: {},
+  };
+
+  // First, verify we're on the happy path for usage and get the fetch URL part
   const href = window.location.href; // Entire browser URL
   const fetchDeclarationIdx = href.indexOf(ROUTES.FETCH_DATA);
   if (fetchDeclarationIdx === -1) {
     // Fetch path not found
-    return "";
+    return MALFORMED_FETCH_RETURN_VAL;
   }
   // Above tells us where fetch path starts in href. To get the URL, we need to
   // skip to its end and then also go 1 farther to pass by the trailing `/`.
-  const fetchUrlIdx = fetchDeclarationIdx + ROUTES.FETCH_DATA.length + 1;
-  const fetchUrl = href.slice(fetchUrlIdx);
-  if (fetchUrl === "") {
+  const fetchHrefIdx = fetchDeclarationIdx + ROUTES.FETCH_DATA.length + 1;
+  const fetchTarget = href.slice(fetchHrefIdx);
+  if (fetchTarget === "") {
     // Fetch path found, but nothing given for data URL
-    return "";
+    return MALFORMED_FETCH_RETURN_VAL;
   }
-  return schemifyUrl(fetchUrl);
+
+  // Separate and handle any search params at end from rest of the fetch URL
+  const [preSearchUrl, searchString] = fetchTarget.split("?");
+  const { galagoParams, remainingSearchString } = extractSearchParams(searchString);
+  let targetUrl = preSearchUrl;
+  if (remainingSearchString) {
+    targetUrl = preSearchUrl + "?" + remainingSearchString;
+  }
+  targetUrl = schemifyUrl(targetUrl); // Ensure the URL we'll fetch has a schme
+
+  return {
+    targetUrl,
+    galagoParams,
+  };
 }
